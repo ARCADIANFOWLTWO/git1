88a8ef7d2d9ea47301c8b57ad666c57bd66d2bc8
ARCADA <taoariya99@gmail.com>
2020-06-24 15:25:12 +0300

    Два java-файла

---
+++ src/com/level/jb2/HelloWorld1.java
@@ -0,0 +1,7 @@
+package com.level.jb2;
+
+public class HelloWorld1 {
+    public static void main(String[] arg){
+        System.out.println("Hello, world");
+    }
+}
---
+++ src/com/level/jb2/HelloWorld2.java
@@ -0,0 +1,7 @@
+package com.level.jb2;
+
+public class HelloWorld2 {
+    public static void main(String[] arg){
+        System.out.println("Hello, world");
+    }
+}
